017f5f2aec0c85c753dd93496b0f59eab0c67331
tsxuehu <tsxuehu@qq.com>
2023-06-27 23:22:28 +0800

    feat: co

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,12 @@
 #include "ui/browserwindow.h"
 
 #include <QApplication>
-
+/**
+ * 三个线程
+ * 1. ui线程，处理ui交互时间
+ * 2. ws线程，处理ws消息
+ * 3. 业务线程， ui线程、ws线程将要做的事情封装成任务丢给业务线程处理
+ */
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
